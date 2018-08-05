940a1c8f0c6aa06270a246a255581dbef41dae0f
Christian Barrios <christiandbf@hotmail.com>
2018-08-05 11:48:50 -0500

    API endpoints

---
+++ src/api.js
@@ -0,0 +1,48 @@
+import { API_KEY, DEFAULT_SEARCH, MAX_RESULTS } from './constants';
+
+const base = 'https://www.googleapis.com/youtube/v3';
+
+const normalize = json => {
+  return json.items.reduce((acc, element) =>
+    [...acc, {
+      id: element.id.videoId,
+      title: element.snippet.title,
+      author: element.snippet.channelTitle,
+      description: element.snippet.description,
+      img: element.snippet.thumbnails.high.url
+    }],
+  []);
+};
+
+export const getVideos = async (term) => {
+  const uri = `${base}/search?key=${API_KEY}&part=snippet&q=${term || DEFAULT_SEARCH}&type=video&maxResults=${MAX_RESULTS}`;
+  try {
+    const response = await fetch(uri);
+    const json = await response.json();
+    return normalize(json);
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getVideoInfo = async (id) => {
+  const uri = `${base}/videos?key=${API_KEY}&part=snippet&statistics&id=${id}&type=video`;
+  try {
+    const response = await fetch(uri);
+    const json = await response.json();
+    return json;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getRecommendedVideos = async (id) => {
+  const uri = `${base}/search?key=${API_KEY}&part=snippet&relatedToVideoId=${id}&type=video&maxResults=${MAX_RESULTS}`;
+  try {
+    const response = await fetch(uri);
+    const json = await response.json();
+    return normalize(json);
+  } catch (err) {
+    throw err;
+  }
+};
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const API_KEY = 'AIzaSyASSJNgzHQQDmhotVtZZwUbDGibRw7OQCc';
+export const DEFAULT_SEARCH = '';
+export const MAX_RESULTS = 10;
